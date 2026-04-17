993142b0ac26a00112ff99b401853b053fbe0286
GWHM0603 <uhmin0012@gmail.com>
2026-04-17 13:33:27 +0900

    함수 기초

---
+++ DAY07/ex_funtion_01.py
@@ -0,0 +1,23 @@
+# ===== ===== ===== ===== =====
+# 함수: def():
+# ===== ===== ===== ===== =====
+
+
+def add1(a, b):
+    return a + b
+print(add1(2, 3))
+
+
+def add2(a, b):
+    print(a + b)
+add2(2, 3)
+
+
+def add3():
+    print(3 + 2)
+add3()
+
+
+def add4():
+    return 3 + 2
+print(add4())
